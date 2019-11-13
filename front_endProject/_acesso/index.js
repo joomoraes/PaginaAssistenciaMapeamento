489644247bebb88105094107aeb039534094ee77
Joao Pedro <52724220+joomoraes@users.noreply.github.com>
2019-11-13 16:59:53 -0400

    Add files via upload

--- front_endProject/_acesso/index.js
+++ front_endProject/_acesso/index.js
@@ -1,15 +1,87 @@
-imagem1 = new Image();
-imagem1.src = “assistencia_estudantil\images\green.png”;
-imagem1on = new Image();
-imagem1on.src = “assistencia_estudantil\images\red.png”;
+function mouseDentro(id)   {	
+            document.getElementById(id).src = "assistencia_estudantil/images/assistencia_estudantil2.png"   
+        } 
+        
+        function mouseFora(id)   {	
+            document.getElementById(id).src = "assistencia_estudantil/images/assistencia_estudantil.png"   
+        }
+        
+        //FUNÇÃO IMAGEM 2
+        
+        function mouseDentro2(id)   {	
+            document.getElementById(id).src = "calendario_academico/images/calendario_academico2.png"   
+        } 
+        
+        function mouseFora2(id)   {	
+            document.getElementById(id).src = "calendario_academico/images/calendario_academico.png"   
+        }
+        
+         //FUNÇÃO IMAGEM 3
+        
+        function mouseDentro3(id)   {	
+            document.getElementById(id).src = "evasao/images/evasao2.png"   
+        } 
+        
+        function mouseFora3(id)   {	
+            document.getElementById(id).src = "evasao/images/evasao.png"   
+        }
+        
+         //FUNÇÃO IMAGEM 4
+        
+        function mouseDentro4(id)   {	
+            document.getElementById(id).src = "trancamento_matricula/images/trancamento_matricula2.png"   
+        } 
+        
+        function mouseFora4(id)   {	
+            document.getElementById(id).src = "trancamento_matricula/images/trancamento_matricula.png"   
+        }
+        
+          //FUNÇÃO IMAGEM 5
+        
+        function mouseDentro5(id)   {	
+            document.getElementById(id).src = "cadastro_curso/images/cadastro_curso2.png"   
+        } 
+        
+        function mouseFora5(id)   {	
+            document.getElementById(id).src = "cadastro_curso/images/cadastro_curso.png"   
+        }
+        
+          //FUNÇÃO IMAGEM 6
+        
+        function mouseDentro6(id)   {	
+            document.getElementById(id).src = "criacao_curso/images/criacao_curso2.png"   
+        } 
+        
+        function mouseFora6(id)   {	
+            document.getElementById(id).src = "criacao_curso/images/criacao_curso.png"   
+        }
+        
+          //FUNÇÃO IMAGEM 7
+        
+        function mouseDentro7(id)   {	
+            document.getElementById(id).src = "justificativa_faltas/images/justificativa_faltas2.png"   
+        } 
+        
+        function mouseFora7(id)   {	
+            document.getElementById(id).src = "justificativa_faltas/images/justificativa_faltas.png"   
+        }
+    
+        //MODO DESKTOP 
+        
+       function changeDesktop(cond) {
+           
+           if(cond == 'true') {
+               window.location.href = "index_desktop.html"
+           }
+       }
 
-function Ativo1(imagem)
-{
-if (document.images) document[imagem].src = eval(imagem + “on.src”);
-}
+       //MODO MOBILE
 
-function Desativo1(imagem)
-{
-if (document.images) document[imagem].src = eval(imagem + “.src”);
-}
+        function changeDesktop2(cond2) {
+           
+           if(cond2 == 'true') {
+               window.location.href = "index.html"
+           }
+       }
 
+        
